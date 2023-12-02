dfb9527162d23c6c89862ef6a16001cce16b0572
Victor Horton <vichorton5@gmail.com>
2023-12-01 18:19:43 -0600

    Add Brew Model and Migration

---
+++ CoffeeApp/Data/Migrations/20231202001606_AddBrewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBrewModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CoffeeApp/Models/Brew.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CoffeeApp.Models
+{
+
+    public enum BrewType
+    {
+        Drip,
+        FrenchPress,
+        PourOver,
+        ColdBrew,
+        Other
+    }
+
+    public class Brew
+    {
+        public int Id { get; set; }
+        public string CoffeeOrigin { get; set; }
+        public BrewType BrewType { get; set; }
+
+        public Brew()
+        {
+        }
+    }
+}
